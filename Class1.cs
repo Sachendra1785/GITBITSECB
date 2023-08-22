a2c04e6eee7d650261a51fd21666dbd71b2c7f87
Sachendra <sachendra.chauhan@gla.ac.in>
2023-08-22 08:28:40 +0530

    Class updatedNew Class AddedAdd project files.

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITBITSECB
+{
+    public class Class1
+    {
+        public void PublicFunc()
+        {
+            Console.WriteLine("Public Function");
+            Console.WriteLine("Class updated");
+        }
+        protected void ProtectedFunc()
+        {
+            Console.WriteLine("Protected Function");
+            Console.WriteLine("Class updated");
+        }
+    }
+    public class Class2
+    {
+        public void PublicFunc() {}
+        private void PrivateFunc() { }
+        protected void ProtectedFunc() { }
+        internal  void InternalFunc() { }
+        protected internal void ProIntFunc() { }
+        private protected void PriProFunc() { }
+
+    }
+}
